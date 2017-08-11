a82de6fca11392ca4d27098da2de8a92f475a3d5
Christian Bay <christianb@unity3d.com>
2017-08-11 13:34:42 +0200

    Remove sql debug messages
    did keep the merge message and fixed the tests

--- master/buildbot/test/unit/test_steps_find_previous_build.py
+++ master/buildbot/test/unit/test_steps_find_previous_build.py
@@ -62,6 +62,7 @@ class TestFindPreviousSuccessfulBuild(steps.BuildStepMixin, config.ConfigErrorsM
         def getSourceValues():
             return [mock.Mock()]
         buildRequest = mock.Mock()
+        buildRequest.id = 1
         buildRequest.sources.values = getSourceValues
         self.build.requests = [
             buildRequest
